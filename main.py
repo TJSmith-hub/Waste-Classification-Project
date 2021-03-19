2c662dcb3d5687201e3990db1df7ec8fd206abd9
AppSmith <tobysmith459@gmail.com>
2021-03-19 18:12:18 +0000

    prediction visualisation and model changes

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ classes = ["cardboard","glass","metal","paper","plastic","trash"]
 
 dataset = pd.dataset()
 
-dataset.resize_images(255,255)
+dataset.resize_images(128,128)
 
 dataset.plot()
 
@@ -32,23 +32,77 @@ print(train_images.shape)
 model = models.Sequential()
 model.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=train_images[0].shape))
 model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(6))
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(6, activation='softmax'))
 model.summary()
 
-model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False), metrics=['accuracy'])
 
-history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels), batch_size=8)
+history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+
+print(test_images[0].shape)
+
+predictions = model.predict(test_images)
+
+#np.set_printoptions(precision=3)
+#print(precticted)
 
 plt.plot(history.history['accuracy'], label='accuracy')
 plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
-plt.ylim([0.5, 1])
+plt.ylim([0, 1])
 plt.legend(loc='lower right')
 plt.show()
 
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
 print(test_acc)
+
+def plot_image(i, predictions_array, true_label, img):
+  true_label, img = true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(classes[predicted_label],
+                                100*np.max(predictions_array),
+                                classes[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  true_label = true_label[i]
+  plt.grid(False)
+  plt.xticks(range(6))
+  plt.yticks([])
+  thisplot = plt.bar(range(6), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
+# Plot the first X test images, their predicted labels, and the true labels.
+# Color correct predictions in blue and incorrect predictions in red.
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions[i], test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
